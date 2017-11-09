9a5c1f8ae4b5b7e50bb4e1205fbebbe15d5b11f4
eresyyl <eresyyl@gmail.com>
2017-11-10 01:51:36 +0700

    v2.1

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const Koa = require('koa'),
 	app = new Koa(),
-	{routes, allowedMethods} = require('./lib/routes'),
+	mongoose = require('mongoose'),
 	agenda = require('./lib/agenda.js'),
-	mongoConnectionString = require('./config/mongo.json').connection_string;
+	{routes, allowedMethods} = require('./lib/routes'),
+	mongoConnectionString = require('./config/mongo.json').connection_string;	
 
-const mongoose = require('./lib/mongoose.js');	
 
-let db = mongoose.createConnection(mongoConnectionString);
+mongoose.connect(mongoConnectionString, {useMongoClient: true});
+let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('Mongoose is on!');
+	console.log('Mongoose is on!');
 });
 	
 app.use(routes());
@@ -18,5 +19,7 @@ app.use(allowedMethods());
 app.listen(3000);
 
 agenda.on('ready', async() =>  {
+	console.log('agenda is ready!');
+	
 	agenda.start();
 });
